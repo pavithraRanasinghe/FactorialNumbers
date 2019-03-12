e39d3a874a689c5d7ab1592b3bdc1e748397721c
PavithraRanasinghe <paviranasinghe20@gamil.com>
2019-03-12 09:23:57 +0530

    finding factorial

--- Factorial.java
+++ Factorial.java
@@ -1,1 +1,16 @@
-
+import java.util.*;
+class Factorial{
+		public static void main(String args[]){
+			Scanner input=new Scanner(System.in);
+			System.out.print("Input a number : ");
+			int x=input.nextInt();
+			int fact=1;
+			System.out.print("Factorial of "+x+"! =");
+			for(int i=x;i>0;i--){
+				System.out.print(i+"*");
+				fact*=i;
+			}
+			System.out.print("\b = ");
+			System.out.println(fact);
+		}
+}
